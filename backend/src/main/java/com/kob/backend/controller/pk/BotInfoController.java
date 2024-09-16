edef9c2aed8a8bebd24463a275608158fc5c8bd7
MaggieXuzy <maggiexuzy@163.com>
2024-09-16 17:36:22 +0800

    basic part finished

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -3,20 +3,21 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
-    public List<String> getBotInfo() {
-        List<String> list = new LinkedList<>();
-        list.add("a");
-        list.add("b");
-        list.add("c");
-        return list;
+    public Map<String, String> getBotInfo() {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("name", "tiger");
+        map.put("rating", "1500");
+        return map;
 
     }
 }
