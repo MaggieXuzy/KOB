e18ff053394f1be2f0b3ba0d64cc87ace6f4155f
MaggieXuzy <maggiexuzy@163.com>
2024-09-18 17:08:26 +0800

    create map

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,7 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PKIndexView from '../views/pk/PKIndexView.vue'
+import RecordIndexView from '../views/record/RecordIndexView.vue'
+import RankListIndexView from '../views/ranklist/RankListIndexView.vue'
+import UserBotIndexView from '../views/user/bots/UserBotIndexView.vue'
+import NotFoundIndex from '../views/error/NotFound.vue'
+
 
 const routes = [
-
+  {
+    path: "/",
+    redirect: "/pk/",
+    name: "home",
+  },
+  {
+    path: "/pk/",
+    name: "pk_index",
+    component: PKIndexView,
+  },
+  {
+    path: "/record/",
+    name: "record_index",
+    component: RecordIndexView,
+  },
+  {
+    path: "/ranklist/",
+    name: "ranklist_index",
+    component: RankListIndexView,
+  },
+  {
+    path: "/user/bot/",
+    name: "user_bot_index",
+    component: UserBotIndexView,
+  },
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFoundIndex,
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404/",
+  }
 ]
 
 const router = createRouter({
---
+++ web/src/views/pk/PKIndexView.vue
@@ -0,0 +1,18 @@
+<template>
+    <PlayGround>PK</PlayGround>
+</template>
+
+<script>
+// import ContentField from "../../components/ContentField.vue"
+import PlayGround from "../../components/PlayGround.vue"
+
+export default {
+    components: {
+        //ContentField,
+        PlayGround
+    }
+}
+</script>
+
+<style scoped>
+</style>
