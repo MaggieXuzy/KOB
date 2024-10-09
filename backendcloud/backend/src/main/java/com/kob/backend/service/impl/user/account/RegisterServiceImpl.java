40e5afa8145393d2220214f70e9164e3d276edde
MaggieXuzy <maggiexuzy@163.com>
2024-10-09 17:25:15 +0100

    finish matching system

---
+++ backendcloud/backend/src/main/java/com/kob/backend/service/impl/user/account/RegisterServiceImpl.java
@@ -0,0 +1,77 @@
+package com.kob.backend.service.impl.user.account;
+
+import com.kob.backend.mapper.UserMapper;
+import com.kob.backend.pojo.User;
+import com.kob.backend.service.user.account.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class RegisterServiceImpl implements RegisterService {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Map<String, String> register(String username, String password, String confirmedPassword) {
+        Map<String, String> map = new HashMap<String, String>();
+        if (username == null) {
+            map.put("error_message", "username can't be null");
+            return map;
+        }
+        if (password == null || confirmedPassword == null) {
+            map.put("error_message", "password can't be null");
+            return map;
+        }
+
+        username = username.trim();
+        if (username.length() == 0) {
+            map.put("error_message", "username can't be empty");
+            return map;
+        }
+
+        if (password.length() == 0 || confirmedPassword.length() == 0) {
+            map.put("error_message", "password can't be empty");
+            return map;
+        }
+
+        if (username.length() > 100) {
+            map.put("error_message", "username too long (can't be greater than 100)`");
+            return map;
+        }
+
+        if (password.length() > 100 || confirmedPassword.length() > 100) {
+            map.put("error_message", "password too long (can't be greater than 100)`");
+            return map;
+        }
+
+        if (!password.equals(confirmedPassword)) {
+            map.put("error_message", "passwords don't match");
+            return map;
+        }
+
+        QueryWrapper<User> queryWrapper = new QueryWrapper();
+        queryWrapper.eq("username", username);
+        List<User> users = userMapper.selectList(queryWrapper);
+        if (!users.isEmpty()) {
+            map.put("error_message", "username already exist");
+            return map;
+        }
+
+        String encodedPassword = passwordEncoder.encode(password);
+        String photo = "https://cdn.acwing.com/media/user/profile/photo/154868_lg_aaa78c287a.jpg";
+        User user = new User(null, username, encodedPassword, photo, 1500);
+        userMapper.insert(user);
+
+        map.put("error_message", "success");
+        return map;
+    }
+}
